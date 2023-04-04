b444cd55f91c28cce0b14d8230966e6985d8a9b2
lamnaourm <mohammed.lamnaour@gmail.com>
2023-04-04 14:11:38 +0000

    models

--- product-service/model/product.js
+++ product-service/model/product.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
       {
-        name: { type: String },
-        description: { type: String },
-        price: { type: Number },
+        name: { type: String, require: true, unique:true },
+        description: { type: String, require: true },
+        price: { type: Number, require: true },
       },
   { collection: "products" }
 );
